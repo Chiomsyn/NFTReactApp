d506fddc7f280cde1c1b14794bce900fad33328e
Chioma Ezeamama <mirianangelk@gmail.com>
2022-08-23 13:20:16 +0100

    corrected a page

--- src/pages/Connect.jsx
+++ src/pages/Connect.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import image5 from '../assests/image5.png'
+import image7 from '../assests/image7.jpg'
+import image8 from '../assests/image8.png'
+import image9 from '../assests/image9.png'
+import image10 from '../assests/image10.png'
+import image11 from '../assests/image11.png'
+import image12 from '../assests/image12.png'
+import image13 from '../assests/image13.png'
 
 const Connect = () => {
   return (
@@ -8,8 +14,26 @@ const Connect = () => {
         <h1 className='text-4xl my-4 font-bold'>Connect with your clients through a custom branded</h1>
         <p className='text-slate-400 my-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nostrum odit dolores.</p>
       </div>
-      <div className='my-6'>
-          <img src={image5} alt='/' className='w-full h-[500px]'/>
+      <div className='my-6 flex relative'>
+          <img src={image7} alt='/' className='w-[100%] h-[500px]'/>
+          <div className='absolute rounded-full bg-[#F7A027] top-[10%] right-[50%]'>
+          <img src={image8} alt='/' className='w-14 h-14 rounded-full'/>
+          </div>
+          <div className='absolute rounded-full bg-[#E0AAFE] top-[30%] right-[20%]'>
+          <img src={image9} alt='/' className='w-14 h-14 rounded-full'/>
+          </div>
+          <div className='absolute rounded-full bg-[#465565] bottom-[30%] left-[20%]'>
+          <img src={image10} alt='/' className='w-14 h-14 rounded-full'/>
+          </div>
+          <div className='absolute rounded-full bg-[#39C29A] bottom-[70%] left-[4%]'>
+          <img src={image11} alt='/' className='w-14 h-14 rounded-full'/>
+          </div>
+          <div className='absolute rounded-full bg-[#1293EC] bottom-[30%] right-[10%]'>
+          <img src={image12} alt='/' className='w-14 h-14 rounded-full'/>
+          </div>
+          <div className='absolute rounded-full bg-[#F8794B] bottom-[50%] left-[50%]'>
+          <img src={image13} alt='/' className='w-14 h-14 rounded-full'/>
+          </div>
       </div>
     </div>
   )
--- src/pages/HeroImage.jsx
+++ src/pages/HeroImage.jsx
@@ -5,15 +5,15 @@ import { ArrowSmDownIcon } from '@heroicons/react/outline'
 const HeroImage = () => {
   return (
     <div className='w-full flex flex-col justify-between overflow-hidden'>
-      <div className='bg-[#140624] sm:max-w-[1980px] h-[320px]  md:h-[420px] xl:h-[500px] 2xl:h-[450px] w-full relative mt-24'>
-        <div className='grid md:grid-cols-3 justify-between md:mx-[30px] lg:ml-[100px] 2xl:ml-[300px] rounded-xl'>
-          <div className='flex-col col-start-1 col-end-3 mx-6 md:mx-16 mt-10 md:mt-20 2xl:mt-20'>
-            <div className=' h-full'>
+      <div className='bg-[#140624]  h-[320px] md:h-[420px] lg:h-[450px] xl:h-[450px] 2xl:h-[450px] w-full relative mt-20'>
+        <div className='grid md:grid-cols-3 justify-between md:ml-[90px] lg:ml-[40px] 2xl:ml-[300px] rounded-xl'>
+          <div className=' col-start-1 col-end-3 mx-6 md:mx-16 mt-10 md:mt-20 2xl:mt-20'>
+            <div className=' h-[50%]'>
             <h1 className='md:text-3xl xl:text-5xl font-bold text-white'>BUY & SELL DIGITAL NFT token</h1>
               <p className='text-slate-400 my-6 text-sm'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere tenetur consectetur ab asperiores, ad esse.</p>
-              <button className='border-none text-white py-4 bg-blue-300'>Get Started Now</button>
+              <button className='border-none text-white pt-4 bg-blue-300'>Get Started Now</button>
             </div>
-            <div className='flex mt-4  md:mt-5 2xl:mt-7 w-full '>
+            <div className='flex  items-center mt-4 md:mt-5 2xl:mt-7 w-full '>
               <div className='flex items-center'>
                 <div className='lg:w-10 lg:h-10 w-7 h-7 rounded-full flex items-center justify-center bg-[#95DED0]'>
                   <ArrowSmDownIcon className='w-7 text-blue-700' />
@@ -21,12 +21,12 @@ const HeroImage = () => {
                 <div><p className='mx-4 w-full text-white'>Scroll Down</p></div>
               </div>
               <div className='-rotate-90 border-t-2 hidden lg:flex  my-10 text-white border-t-green-600'><p className='w-full'>2u + Uturu worldwide</p></div>
-              <div className=' px-4 xl:px-0 w-[70%] xl:w-[40%] items-end h-full flex xl:items-end'>
-                <p className='text-slate-400'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit amet.</p> </div>
+              <div className=' px-4 xl:px-0 w-[150px]'>
+                <p className='text-slate-400'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p> </div>
             </div>
           </div>
-          <div className='h-full'>
-            <img className='hidden md:flex xl:w-[220px] mt-40' src={image1} alt="/" />
+          <div className='h-full '>
+            <img className='hidden md:flex items-center w-[70%] mt-40' src={image1} alt="/" />
           </div>
         </div>
         <div className='hidden md:flex left-[35%] top-6 lg:top-[15%] w-4 h-4 rounded-full bg-green-500 shadow-slate-600 shadow-md absolute'></div>
